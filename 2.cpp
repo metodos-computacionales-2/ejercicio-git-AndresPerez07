f3ab1fa0a2f940ee2dc5fca6699a9c744279e32b
Andres Perez <ac.perez1491@uniandes.edu.co>
2019-10-31 21:52:55 -0500

    Se encontro el numero mayor

--- 2.cpp
+++ 2.cpp
@@ -10,11 +10,19 @@ cout << "Ingrese tres numeros enteros" << endl;
 cin >> nums[0];
 cin >> nums[1];
 cin >> nums[2];
-
+int cont = nums[0];
+int ix = 0;
 for (int i = 0; i < 3; i++) {
   cout << "num" << i+1 << "= " << nums[i] << endl;
-}
+  if (nums[i]> cont)
+  {
+    cont = nums[i];
+    ix ++;
 
+  }
+
+}
+cout << "El numero mayor es: " << nums[ix] << endl;
 
   return 0;
 }
