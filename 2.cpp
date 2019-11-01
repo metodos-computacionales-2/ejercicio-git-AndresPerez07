03c922224af88e37054219dd139033474b304c37
Andres Perez <ac.perez1491@uniandes.edu.co>
2019-10-31 22:19:27 -0500

    Arreglo en num mayor y crea catetos

--- 2.cpp
+++ 2.cpp
@@ -10,19 +10,35 @@ cout << "Ingrese tres numeros enteros" << endl;
 cin >> nums[0];
 cin >> nums[1];
 cin >> nums[2];
-int cont = nums[0];
+int cont = 0;
 int ix = 0;
 for (int i = 0; i < 3; i++) {
   cout << "num" << i+1 << "= " << nums[i] << endl;
   if (nums[i]> cont)
   {
     cont = nums[i];
-    ix ++;
+    ix = i;
 
   }
 
 }
 cout << "El numero mayor es: " << nums[ix] << endl;
+int h = nums[ix];
+int *cat = new int [2];
+int j=0;
+int k=0;
+while(j < 3)
+{
+  int temp = nums[j];
+  if (temp != h)
+  {
+    cat[k] = temp;
+    k++;
+  }
+  j++;
+}
+cout << "catetos: " << cat[0] << ", " << cat[1] << endl;
+
 
   return 0;
 }
